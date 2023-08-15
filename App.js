5f852570a9030263714e3064c98775260c211e49
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-08-15 18:38:11 +0300

    navigation

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet } from "react-native";
 import { PostsScreen } from "./Screens/PostsScreen";
+import { CommentsScreen } from "./Screens/CommentsScreen";
+import { Home } from "./Screens/Home";
+import { CreatePostsScreen } from "./Screens/CreatePostsScreen";
+import { MapScreen } from "./Screens/MapScreen";
+import { ProfileScreen } from "./Screens/ProfileScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+// import { LoginScreen } from "./components/LoginScreen";
+// import { RegistrationScreen  } from "./components/RegistrationScreen ";
+
+const MainStack = createStackNavigator();
 
 export default function App() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <MainStack.Navigator>
+        <MainStack.Screen name="PostsScreen" component={PostsScreen} />
+        <MainStack.Screen name="Home" component={Home} />
+        <MainStack.Screen name="CommentsScreen" component={CommentsScreen} />
+        <MainStack.Screen
+          name="CreatePostsScreen"
+          component={CreatePostsScreen}
+        />
+        <MainStack.Screen name="MapScreen" component={MapScreen} />
+        <MainStack.Screen name="ProfileScreen" component={ProfileScreen} />
 
-      <PostsScreen />
-    </ScrollView>
+        {/* <PostsScreen /> */}
+        {/* <MainStack.Screen name="Login" component={Login} /> */}
+        {/* <MainStack.Screen name="Registration" component={Registration} /> */}
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    margin: 0,
-    padding: 0,
-  },
-});
--- Screens/PostsScreen.jsx
+++ Screens/PostsScreen.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { View } from "react-native";
+import { ScrollView, StatusBar, StyleSheet, View } from "react-native";
 import { RegistrationScreen } from "../components/RegistrationScreen";
 import { LoginScreen } from "../components/LoginScreen";
 
 export const PostsScreen = () => {
   return (
     <View>
-      <RegistrationScreen />
-      <LoginScreen />
+      <StatusBar style="auto" />
+      <ScrollView contentContainerStyle={styles.container}>
+        <RegistrationScreen />
+        <LoginScreen />
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    margin: 0,
+    padding: 0,
+  },
+});
